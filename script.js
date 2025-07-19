5f47bdc4c583887a0dca90d0c15897f64822c980
Anshuman <matainja0130@gmail.com>
2025-07-19 14:11:18 +0530

    changes js css

--- script.js
+++ script.js
@@ -3,12 +3,19 @@ const slides = document.getElementsByClassName("slide");
 
 function showSlides() {
   for (let i = 0; i < slides.length; i++) {
-    slides[i].style.opacity = 0;
+    slides[i].classList.remove("active");
   }
+
   slideIndex++;
   if (slideIndex > slides.length) slideIndex = 1;
-  slides[slideIndex - 1].style.opacity = 1;
+
+  slides[slideIndex - 1].classList.add("active");
+
   setTimeout(showSlides, 4000);
 }
 
-window.onload = showSlides;
+// Immediately activate the first slide on load
+window.addEventListener("load", () => {
+  slides[0].classList.add("active");
+  showSlides();
+});
